6e04315ea26d1a67d06fc65ae5f627a9d626bd46
kijima-riku <riku.bulls.0116@icloud.com>
2024-11-26 15:51:00 +0900

    タスクのupdate機能を追加

--- src/main/java/com/sample/spring/tasklist/TaskListDao.java
+++ src/main/java/com/sample/spring/tasklist/TaskListDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,6 +44,14 @@ public class TaskListDao {
 
     public int delete(String id) {
         int number = jdbcTemplate.update("DELETE FROM tasklist WHERE id = ?", id);
+        return number;
+    }
+
+    public int update(TaskItem taskItem) {
+        int number = jdbcTemplate.update(
+                "UPDATE tasklist SET task = ?, deadline = ?, done = ? WHERE id = ?",
+                taskItem.task(), taskItem.deadline(), taskItem.done(), taskItem.id()
+        );
         return number;
     }
 }
